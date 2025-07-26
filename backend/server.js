0bd4a4112579d8fdd17c06d9fb10ef3ef40ddfac
pradyumnajavalagi <ajpradyumna@gmail.com>
2025-07-26 11:42:13 +0530

    added cors

--- backend/server.js
+++ backend/server.js
@@ -2,21 +2,17 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 import todoRoutes from "./routes/todo.route.js";
+import cors from "cors";
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 9000;
 
 app.use(express.json());
+app.use(cors());
 app.use("/api/todos", todoRoutes);
 
-<<<<<<< HEAD
-app.listen(port, '0.0.0.0', () => {
-  connectDB();
-  console.log(`Server started at http://0.0.0.0:${port}`);
-=======
 app.listen(port, "0.0.0.0", () => {
 	connectDB();
 	console.log(`Server started at http://localhost:${port}`);
->>>>>>> 61a4d1eb9e651281b2f1f0e0915c3779f890e551
 });
